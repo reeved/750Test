14e245968ee5aa88c231827cf61742c33055f60b
reeved <dcunhareeve@gmail.com>
2021-05-12 19:07:31 +1200

    initial button event handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import axios from 'axios';
 import styles from './App.module.css';
 
 function App() {
+  const dbServerURI = 'http://localhost:3001';
   const pressed = 0;
+
+  const handleClick = () => {
+    axios.get(`/21`).then((response) => console.log(response));
+  };
 
   return (
     <div className={styles.container}>
-      <div className={`${styles.button} ${pressed ? styles.pressed : ''}`}>
+      <div
+        className={`${styles.button} ${pressed ? styles.pressed : ''}`}
+        onClick={() => handleClick()}
+      >
         {pressed ? `Button is Pressed` : 'Press button!'}
       </div>
       <div className={styles.credits}>
