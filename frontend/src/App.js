de20cad244e6ebd3e0a701b06501e7be9201b439
reeved <dcunhareeve@gmail.com>
2021-05-12 16:49:37 +1200

    added credits on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <div>
-        <h1>HELLO</h1>
-      </div>
       <div className={`${styles.button} ${pressed ? styles.pressed : ''}`}>
         {pressed ? `Button is Pressed` : 'Press button!'}
+      </div>
+      <div className={styles.credits}>
+        <h4>Made by: Reeve D'Cunha (rdcu227)</h4>
       </div>
     </div>
   );
