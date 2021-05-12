3960ecdf40f889585a7a8c3cff2cbaa27c5e27df
reeved <dcunhareeve@gmail.com>
2021-05-12 20:07:33 +1200

    add bootstrap to frontend with loading animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,36 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Spinner from 'react-bootstrap/Spinner';
 
 function App() {
   const [pressed, setPressed] = useState(null);
 
-  const handleClick = () => {
+  const getButtonState = () => {
     axios.get(`/21`).then((response) => {
       console.log(response.data);
-      setPressed(!response.data.state);
+      setPressed(response.data.state);
     });
+  };
+
+  const setButtonState = () => {
     const body = {
       newState: !pressed,
     };
     axios.put(`/21`, body);
   };
+
+  const handleClick = () => {
+    getButtonState();
+    setButtonState();
+    getButtonState();
+  };
+
+  useEffect(() => {
+    console.log('Use Effect ran.');
+    getButtonState();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -22,7 +38,16 @@ function App() {
         className={`${styles.button} ${pressed ? styles.pressed : ''}`}
         onClick={() => handleClick()}
       >
-        {pressed ? `Button is Pressed` : 'Press button!'}
+        {pressed === null ? (
+          <>
+            <Spinner animation='border' />
+            <span>Loading...</span>
+          </>
+        ) : pressed ? (
+          `Button is Pressed`
+        ) : (
+          'Press button!'
+        )}
       </div>
       <div className={styles.credits}>
         <h4>Made by: Reeve D'Cunha (rdcu227)</h4>
